7ca7b3f59d901bc53c22e8e963b25e8c569c26f7
Suriyan Laohaprapanon <suriyant@gmail.com>
2022-08-13 13:37:38 +0700

    add simple setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='R421AXX-modbus-relay',
+      version='1.0',
+      description='R421AXX relay board control',
+      author='Suriyan Laohaprapanon',
+      author_email='suriyant@gmail.com',
+      url='https://github.com/gooroopy/R421A08-rs485-8ch-relay-board',
+      packages=['relay_boards', 'relay_modbus'],
+      install_requires=[
+          'pyserial',
+      ],
+     )
